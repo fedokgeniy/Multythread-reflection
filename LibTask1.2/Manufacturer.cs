3e4b4e361740a8a6235f146e336a231cfefe5f1e
Vladislav Stepanenko <fedkinapochta@gmail.com>
2025-03-24 11:49:58 +0200

    Goooool

---
+++ LibTask1.2/Manufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTask2
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        private bool IsAChildCompany { get; set; }
+
+        public static Manufacturer Create(string name, string address, bool isAChildCompany)
+        {
+            return new Manufacturer { Name = name, Address = address, IsAChildCompany = isAChildCompany };
+        }
+
+        public void PrintObject()
+        {
+            Console.WriteLine($"Manufacturer: Name={Name}, Address={Address}, IsAChildCompany=HIDDEN");
+        }
+    }
+}
---
+++ LibTask1.2/Phone.cs
@@ -0,0 +1,21 @@
+﻿namespace LibTask2
+{
+    public class Phone
+    {
+        private int ID { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+        public string PhoneType { get; set; }
+
+        public static Phone Create(int id, string model, string serialNumber, string phoneType)
+        {
+            return new Phone { ID = id, Model = model, SerialNumber = serialNumber, PhoneType = phoneType };
+        }
+
+        public void PrintObject()
+        {
+            Console.WriteLine($"Phone: ID=HIDDEN, Model={Model}, SerialNumber={SerialNumber}, PhoneType={PhoneType}");
+        }
+    }
+
+}
